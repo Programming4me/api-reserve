460d2449aa52b6f9a7b8a0d3b44a63150e996aac
john <jonmiami4@gmail.com>
2019-12-27 02:35:45 -0800

    change htacess

--- Api/send.php
+++ Api/send.php
@@ -1,5 +1,5 @@
 <?php
-require_once ('Database.php');
+require_once ('../Database.php');
 $args = ['localhost'=> 'localhost', 'username'=> 'hooshman_user', 'password'=> 'mahdi@0912', 'database'=> 'hooshman_test']; // اطلاعات مربوط به دیتابیس
 $db = new Database($args);
 $arr = [
