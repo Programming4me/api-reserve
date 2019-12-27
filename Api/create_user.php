0ae2499f92bc83acfae89c9ee416bc1e8ef0d02c
john <jonmiami4@gmail.com>
2019-12-27 07:12:22 -0800

    font

--- Api/create_user.php
+++ Api/create_user.php
@@ -13,7 +13,7 @@ $result = $db->insertUserByUsername('username', [
 $arr = [
     'status' => true,
     'data' =>
-        array_combine($db->select('users')->fetch_fields(), $db->select('users')->fetch_all())
+        array_combine($db->select('users')->fetch_fields(), $db->select('users')->fetch_array())
     ,
 ];
 
