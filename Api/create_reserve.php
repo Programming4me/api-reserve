645aa1974ec1e3bc168ee08fee84fce2d19d04b8
john <jonmiami4@gmail.com>
2019-12-27 04:37:11 -0800

    change htacess

--- Api/create_reserve.php
+++ Api/create_reserve.php
@@ -3,7 +3,7 @@
 require_once('../funcs.php');
 
   $req = $_REQUEST;
-  dd($req);
+  dd(getallheaders());
 $result = $db->insert('reports', [
     'username' => $_GET['username'],
     'username' => $_GET['username'],
