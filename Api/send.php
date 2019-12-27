351d63bd46628e8f487205c3eac60ff130157a66
john <jonmiami4@gmail.com>
2019-12-27 04:04:22 -0800

    change htacess

--- Api/send.php
+++ Api/send.php
@@ -1,16 +1,24 @@
 <?php
 require_once('../funcs.php');
-dd($_GET['username']);
-$db->insertUserByUsername('username', [
-    'username' => "09361722174",
+
+$result = $db->insertUserByUsername('username', [
+    'username' => $_GET['username'],
     'access_token' => bin2hex(openssl_random_pseudo_bytes(64)),
-    'first_name' => "mehdi",
-    'last_name' => "shahpoury",
-    'password'=> password_hash('123',PASSWORD_DEFAULT)
+    'first_name' => $_GET['first_name'],
+    'last_name' => $_GET['last_name'],
+    'password' => password_hash($_GET['password'], PASSWORD_DEFAULT)
 
 ]);
-$arr = [
-    'status' => true,
-    'data' => [$db->select('users')]
-];
+
+if ($result)
+    $arr = [
+        'status' => true,
+        'data' => [$db->select('users')]
+    ];
+else
+    $arr = [
+        'status' => false,
+        'data' => [$db->select('users')]
+    ];
+
 echo json_encode($arr);
