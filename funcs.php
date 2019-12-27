084826f219f1c1deb466333f86e304c092733319
john <jonmiami4@gmail.com>
2019-12-27 03:18:50 -0800

    change htacess

--- funcs.php
+++ funcs.php
@@ -113,10 +113,23 @@ function p2f($string)
     return curl_exec($ch);
 }
 
-function dd()
+/**
+ * Dumps a given variable along with some additional data.
+ *
+ * @param mixed $var
+ * @param bool $pretty
+ */
+function dd($var, $pretty = false)
 {
-    array_map(function ($x) {
-        var_dump($x);
-    }, func_get_args());
+    $backtrace = debug_backtrace();
+    echo "\n<pre>\n";
+    if (isset($backtrace[0]['file'])) {
+        echo $backtrace[0]['file'] . "\n\n";
+    }
+    echo "Type: " . gettype($var) . "\n";
+    echo "Time: " . date('c') . "\n";
+    echo "---------------------------------\n\n";
+    ($pretty) ? print_r($var) : var_dump($var);
+    echo "</pre>\n";
     die;
 }
