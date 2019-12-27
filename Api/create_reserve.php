acddbd1f8d3e437ec3f946e6be8649bb7c01c6d9
john <jonmiami4@gmail.com>
2019-12-27 04:42:13 -0800

    change htacess

--- Api/create_reserve.php
+++ Api/create_reserve.php
@@ -2,26 +2,23 @@
 
 require_once('../funcs.php');
 
-  $req = $_REQUEST;
-  dd(getallheaders()['token']);
+$req = $_REQUEST;
+$user = $db->getUserByToken(getallheaders()['token']);
 $result = $db->insert('reports', [
-    'username' => $_GET['username'],
-    'username' => $_GET['username'],
-    'username' => $_GET['username'],
-    'username' => $_GET['username'],
-
-
+    'user_id' => $user->id,
+    'start_time' => $_GET['start_time'],
+    'end_time' => $_GET['end_time'],
 ]);
 
 if ($result)
     $arr = [
         'status' => true,
-        'data' => [$db->select('users')]
+        'data' => [$db->select('reports')->fetch_all()]
     ];
 else
     $arr = [
         'status' => false,
-        'data' => [$db->select('users', null, '*')]
+        'data' => [$db->select('users')->fetch_all()]
     ];
 
 echo json_encode($arr);
