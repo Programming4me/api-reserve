e545185217ac7f149f23fd70f510d93a5add6c0b
john <jonmiami4@gmail.com>
2019-12-27 04:52:43 -0800

    change query

--- Database.php
+++ Database.php
@@ -123,6 +123,7 @@ class Database extends mysqli
     public function getUserByToken($token)
     {
         $res = $this->select('users', "access_token='$token'",'id');
+        dd($res);
         if (!$this->row_exists($res))  dd($res);//return $res->fetch_assoc();
         return false;
     }
